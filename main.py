25e44a5c3b1ec71c04baadf25bd412edf0222c19
barakadanny <barakadan421@gmail.com>
2024-01-17 20:16:01 +0300

    scrap movies and reverse array

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
-
 response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
 movies_web_text = response.text
 
 soup = BeautifulSoup(movies_web_text, "html.parser")
 movies = soup.find_all(name="h3", class_="title")
-print(movies)
 
+movies_array = []
+for movie in movies:
+    movie_text = movie.text
+    movies_array.append(movie_text)
 
+res = movies_array[::-1]
+print(res)
