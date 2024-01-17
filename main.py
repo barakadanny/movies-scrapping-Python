212a41ca01c843ca31c05602f10b157e67d03195
barakadanny <barakadan421@gmail.com>
2024-01-17 20:07:34 +0300

    request data & BeautifulSoup

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+# URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
-# Write your code below this line 👇
+response = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+movies_web_text = response.text
+
+soup = BeautifulSoup(movies_web_text, "html.parser")
+movies = soup.find_all(name="h3", class_="title")
+print(movies)
 
 
